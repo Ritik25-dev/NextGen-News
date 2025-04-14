adc0005040e4db301d1006e3eb8b65ae5f729ad5
Ritik <devit3900@gmail.com>
2025-04-14 16:36:51 +0530

    First commit - pushing NextGen News project

---
+++ script.js
@@ -0,0 +1,32 @@
+const api = "52ee31fb251395c29b8d163d39c07094";
+const url =`https://gnews.io/api/v4/top-headlines?lang=en&country=in&topic=technology&max=30&token=${api}`;
+
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    let allArticles = data.articles;
+    allArticles.forEach(article => {
+        let title = article.title;
+        let description = article.description;
+        let img = article.image;
+        let link = article.url;
+        let date = article.publishedAt;
+        createNews(title,description,img,link,date);
+     });
+  });
+
+
+
+function createNews(title,description,img,link,date){
+    document.querySelector('.news-container').innerHTML += `<div class="news-card">
+                <div class="text">
+                    <h2>${title}</h2>
+                    <h4>Published On: ${date}</h4>
+                    <p>${description}</p>
+                    <a href="${link}" target="_main">Read more</a>
+                </div>
+                    <img src="${img}" alt="Image" >
+                    
+                </div>`;
+}
